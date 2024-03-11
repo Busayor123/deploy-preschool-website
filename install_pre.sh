7f8aacb96c8c68153f189fd40f4619bd919a0e52
Busayo Abati <abatiaisha@yahoo.com>
2024-03-11 09:49:57 -0400

    save

--- install_pre.sh
+++ install_pre.sh
@@ -3,7 +3,7 @@ sudo yum update -y
 sudo yum install -y httpd
 cd /var/www/html 
 wget https://github.com/Busayor123/Html-website/raw/main/xmen-main.zip
-unzip xmen.main.zip 
+unzip xmen-main.zip 
 cp -r techmax-main/* /var/www/html/ 
 rm -rf xmen-main.zip 
 systemctl enable httpd
