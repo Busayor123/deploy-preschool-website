26f6c86f8b687e28d8de4a14e36feecdeb1e8a54
Busayo Abati <abatiaisha@yahoo.com>
2024-03-11 09:35:06 -0400

    save

--- install_pre.sh
+++ install_pre.sh
@@ -1,12 +1,10 @@
-
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
 cd /var/www/html 
-wget https://github.com/Busayor123/deploy-preschool-website/raw/main/kider.zip
-unzip kider.zip
-sudo cp -r preschool-website-template/*  /var/www/html
-rm -rf kider.zip
+wget https://github.com/Busayor123/Html-website/raw/main/xmen-main.zip
+unzip xmen.main.zip 
+cp -r techmax-main/* /var/www/html/ 
+rm -rf xmen-main.zip 
 systemctl enable httpd
 systemctl start httpd
-
--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ module "ec2_instance" {
   vpc_security_group_ids      = [module.Jenkins_sg.security_group_id]
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  user_data                   = file("installme.sh")
+  user_data                   = file("install_pre.sh")
   availability_zone           = data.aws_availability_zones.AZS.names[0]
 
 
@@ -78,3 +78,168 @@ module "ec2_instance" {
 output "public_ip" {
   value = module.ec2_instance.public_ip
 }
+
+
+
+# module "alb" {
+#   source = "terraform-aws-modules/alb/aws"
+
+#   name    = "my-alb"
+#   vpc_id  = module.vpc.vpc_id
+#   subnets =  var.public_subnets
+
+#   # Security Group
+#   security_group_ingress_rules = {
+#     all_http = {
+#       from_port   = 80
+#       to_port     = 80
+#       ip_protocol = "tcp"
+#       description = "HTTP web traffic"
+#       cidr_ipv4   = "0.0.0.0/0"
+#     }
+#     # all_https = {
+#     #   from_port   = 443
+#     #   to_port     = 443
+#     #   ip_protocol = "tcp"
+#     #   description = "HTTPS web traffic"
+#     #   cidr_ipv4   = "0.0.0.0/0"
+#     # }
+#   }
+#   security_group_egress_rules = {
+#     all = {
+#       ip_protocol = "-1"
+#       cidr_ipv4   = "10.0.0.0/17"
+#     }
+#   }
+
+#   access_logs = {
+#     bucket = "my-alb-logs"
+#   }
+
+#   # listeners = {
+#   #   ex-http-https-redirect = {
+#   #     port     = 80
+#   #     protocol = "HTTP"
+#   #     redirect = {
+#   #       port        = "443"
+#   #       protocol    = "HTTPS"
+#   #       status_code = "HTTP_301"
+#   #     }
+#   #   }
+#   #   ex-https = {
+#   #     port            = 443
+#   #     protocol        = "HTTPS"
+#   #     certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+#   #     forward = {
+#   #       target_group_key = "ex-instance"
+#   #     }
+#   #   }
+#   # }
+
+#   target_groups = {
+#     ex-instance = {
+#       name_prefix      = "h1"
+#       protocol         = "HTTP"
+#       port             = 80
+#       target_type      = "instance"
+#     }
+#   }
+
+#   tags = {
+#     Environment = "Development"
+#     Project     = "Example"
+#   }
+# }
+
+
+# resource "aws_security_group" "alb_sg" {
+#   name        = "ELb sg"
+#   vpc_id      = module.vpc.vpc_id
+
+#   // Ingress rules
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   # // Add more ingress rules based on the variable
+#   # dynamic "ingress" {
+#   #   for_each = var.ingress_rules
+#   #   content {
+#   #     from_port   = ingress.value.from_port
+#   #     to_port     = ingress.value.to_port
+#   #     protocol    = ingress.value.protocol
+#   #     cidr_blocks = ingress.value.cidr_blocks
+#   #   }
+#   }
+
+#   // Egress rules
+#   egress {
+#     from_port       = 0
+#     to_port         = 0
+#     protocol        = "-1"
+#     cidr_blocks     = ["0.0.0.0/0"]
+#   }
+
+#   # // Add more egress rules based on the variable
+#   # dynamic "egress" {
+#   #   for_each = var.egress_rules
+#   #   content {
+#   #     from_port   = egress.value.from_port
+#   #     to_port     = egress.value.to_port
+#   #     protocol    = egress.value.protocol
+#   #     cidr_blocks = egress.value.cidr_blocks
+#   #   }
+
+
+
+
+# resource "aws_lb" "test" {
+#   name               = "ALB"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.alb_sg.id]
+#   subnets            = var.public_subnets
+
+
+#   # access_logs {
+#   #   bucket  = aws_s3_bucket.lb_logs.id
+#   #   prefix  = "test-lb"
+#   #   enabled = true
+#   # }
+
+#   tags = {
+#     Environment = "production"
+#   }
+# }
+
+# resource "aws_lb" "front_end" {
+#   # ...
+# }
+
+# resource "aws_lb_target_group" "front_end" {
+#   # ...
+# }
+
+# resource "aws_lb_listener" "front_end" {
+#   load_balancer_arn = aws_lb.front_end.arn
+#   port              = "443"
+#   protocol          = "HTTPS"
+#   # ssl_policy        = "ELBSecurityPolicy-2016-08"
+#   # certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.front_end.arn
+#   }
+# }
+
+# resource "aws_lb_target_group" "TG" {
+#   name     = "ALB TG"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = module.vpc.vpc_id
+# }
